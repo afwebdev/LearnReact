3f1a46877b37b4c21e6d1a2cc19f64bd34e497f0
AFWebDev <andrewfariaj@gmail.com>
2019-05-21 22:56:55 -0400

    Updates

--- precache-manifest.7e73a1edc56e5b4b0ed6547d67cfdf89.js
+++ precache-manifest.7e73a1edc56e5b4b0ed6547d67cfdf89.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "39e1d1c799db66fd2628db71a8034784",
+    "revision": "dec0d5c74516bd074819f43774b6d130",
     "url": "/LearnReact/index.html"
   },
   {
-    "revision": "9aaed69f9a06ea7cf250",
+    "revision": "8b828e74390fc83a28b0",
     "url": "/LearnReact/static/css/main.600cb0f4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/LearnReact/static/js/2.9b3d9aae.chunk.js"
   },
   {
-    "revision": "9aaed69f9a06ea7cf250",
-    "url": "/LearnReact/static/js/main.7a13c8fb.chunk.js"
+    "revision": "8b828e74390fc83a28b0",
+    "url": "/LearnReact/static/js/main.f305a62e.chunk.js"
   },
   {
     "revision": "e1895c5c3183c79ebb77",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/LearnReact/precache-manifest.eba409c2772c5dab0b3097836ec3368d.js"
+  "/LearnReact/precache-manifest.7e73a1edc56e5b4b0ed6547d67cfdf89.js"
 );
 
 self.addEventListener('message', (event) => {
