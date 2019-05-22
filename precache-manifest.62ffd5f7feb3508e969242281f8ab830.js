0f2ba91398ac5a056775b041ee035fb7d791aed6
AFWebDev <andrewfariaj@gmail.com>
2019-05-21 22:43:53 -0400

    Updates

---
+++ precache-manifest.62ffd5f7feb3508e969242281f8ab830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "367ad66105f81b76208f16e256ef4e00",
+    "url": "/LearnReact/index.html"
+  },
+  {
+    "revision": "69238242f3c57971be10",
+    "url": "/LearnReact/static/css/main.600cb0f4.chunk.css"
+  },
+  {
+    "revision": "18a51857fdd037e255b0",
+    "url": "/LearnReact/static/js/2.9b3d9aae.chunk.js"
+  },
+  {
+    "revision": "69238242f3c57971be10",
+    "url": "/LearnReact/static/js/main.c08b768c.chunk.js"
+  },
+  {
+    "revision": "e1895c5c3183c79ebb77",
+    "url": "/LearnReact/static/js/runtime~main.a72445d7.js"
+  }
+]);
